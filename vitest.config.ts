70d1534a153ec0f58d578f071ec72c6bcb05de2e
beeetbock <beeetbock2@gmail.com>
2026-03-14 17:21:39 +0530

    Hianime.cv Compatability

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        name: "aniwatch",
+        name: "beataniwatch",
         environment: "node",
         testTimeout: 15000,
         reporters: "default",
